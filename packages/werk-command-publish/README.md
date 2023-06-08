1723f5d3632ff59b7c5a198eb124c6502dbe4edd
Chris Ackerman <chris@topher.land>
2023-06-08 16:51:46 -0700

    feat: add publish command

---
+++ packages/werk-command-publish/README.md
@@ -0,0 +1,67 @@
+# Werk Run Command
+
+Publish workspaces.
+
+Only versions that do not exist in the registry are published.
+
+## Install
+
+```sh
+npm i -D @werk/cli @werk/command-publish
+```
+
+## Publish
+
+The state of the monorepo is [validated](#validation) before publishing. Publishing workspaces with local dependencies is always dependent on the local dependency workspaces also being published successfully, even if the Werk `--no-wait` option is set.
+
+```sh
+werk publish
+```
+
+The packages to be published can be limited using [Werk global options](https://www.npmjs.com/package/@werk/cli#command-line-options). Selected workspaces will still be checked for publishability, and may be skipped if they are not publishable.
+
+```sh
+werk -w my-workspace publish
+```
+
+To pack packages instead of publishing them immediately, set the `--to-archive` option.
+
+```sh
+werk publish --to-archive
+```
+
+The resulting archives can be published later by setting the `--from-archive` option. This will skip most validation, only checking for the existence of archives with a name and version matching the adjacent `package.json` file.
+
+```sh
+werk publish --from-archive
+```
+
+## Dry run
+
+Use the `--dry-run` option to validate without making any permanent changes on disk (temporary only) or pushing to the registry. The `--dry-run` option will be passed through to the `npm publish` or `npm pack` command.
+
+```sh
+werk publish --dry-run
+```
+
+## Validation
+
+Validation is always performed before publishing. This provides reasonable certainty that published packages will have resolvable dependencies based on the expected source code.
+
+1. **Ensure the Git working tree is clean.**
+
+   - If there are uncommitted changes, then there may not be any permanent record of the published code. Publishing also makes temporary changes to `package.json` files, which need to be removed be resetting the uncommitted changes.
+
+2. **Ensure local dependencies are either successfully published, or were already published from up-to-date code.**
+
+   - If there are local modifications that are not already published or going to be published (no version change or filtered out), then local building and testing may not reflect the published behavior of a package.
+
+## Temporary `package.json` changes
+
+The following changes are made before publishing, and will be rolled back after publishing finishes (successfully or not). Change are only made in publishable workspaces.
+
+- Record the current commit hash under the `gitHead` key.
+  - NPM should already do this for publishing. However, it is not done when packing. The field is also poorly documented even though it was added in v7, so it's possible that it might not be added in the future.
+- Update all local workspace dependency versions.
+  - Replace any `file:` or `*` "versions" with real versions.
+  - Update any version ranges so that the minimum version is the current version.
---
+++ packages/werk-command-publish/src/main.ts
@@ -0,0 +1,162 @@
+import assert from 'node:assert';
+import { stat } from 'node:fs/promises';
+import { join } from 'node:path';
+
+import { createCommand, type MutablePackageJson, type Workspace, type WorkspaceContext } from '@werk/cli';
+
+const resultPromises = new Map<string, Promise<{ isPublished: boolean }>>();
+const workspacesToRestore: Workspace[] = [];
+
+export default createCommand({
+  init: ({ commander, command }) => {
+    return commander
+      .description(command.packageJson.description ?? '')
+      .description('Only versions that do not exist in the registry are published.')
+      .option('--to-archive', 'Pack each workspace into an archive.')
+      .addOption(
+        commander.createOption('--from-archive', 'Publish pre-packed workspace archives.').conflicts('toArchive'),
+      )
+      .option('--dry-run', 'Perform a dry run for validation.');
+  },
+  each: async (context) => {
+    const promise = each(context);
+
+    resultPromises.set(
+      context.workspace.name,
+      promise.catch(() => ({ isPublished: false })),
+    );
+
+    await promise;
+  },
+  cleanup: ({ log, spawn }) => {
+    workspacesToRestore.forEach(({ name, dir }) => {
+      if (spawn('git', ['restore', '--source=HEAD', '--staged', '--worktree', '--', dir]).failed()) {
+        log.debug(`Failed to restore workspace "${name}".`);
+      }
+    });
+  },
+});
+
+const each = async (
+  context: WorkspaceContext<[], { toArchive?: true; fromArchive?: true; dryRun?: true }>,
+): Promise<{ isPublished: boolean }> => {
+  const { opts, workspace } = context;
+
+  if (!workspace.selected) return { isPublished: false };
+
+  if (opts.fromArchive) {
+    return await publishFromArchive(context);
+  } else {
+    return await publishFromFilesystem(context);
+  }
+};
+
+const publishFromArchive = async (
+  context: WorkspaceContext<[], { dryRun?: true }>,
+): Promise<{ isPublished: boolean }> => {
+  const { log, opts, workspace, spawn } = context;
+  const { dryRun = false } = opts;
+  const filename = join(workspace.dir, `${workspace.name.replace(/^@/u, '').replace(/\//gu, '-')}.tgz`);
+  const filenameExists = await stat(filename)
+    .then((stats) => stats.isFile())
+    .catch(() => false);
+
+  if (!filenameExists) {
+    log.debug(`Skipping workspace "${workspace.name}@${workspace.version}" because the archive file is missing.`);
+    return { isPublished: false };
+  }
+
+  await spawn('npm', ['publish', ...(dryRun ? ['--dry-run'] : []), filename], { errorEcho: true, errorThrow: true });
+
+  return { isPublished: true };
+};
+
+const publishFromFilesystem = async (
+  context: WorkspaceContext<[], { toArchive?: true; dryRun?: true }>,
+): Promise<{ isPublished: boolean }> => {
+  const { log, opts, workspace, spawn } = context;
+
+  if (await workspace.getNpmIsPublished()) {
+    log.debug(`Skipping workspace "${workspace.name}@${workspace.version}" because it is already published.`);
+    return { isPublished: false };
+  }
+
+  const isGitRepo = await workspace.getGitIsRepo();
+
+  if (isGitRepo) {
+    const [isClean, isModified] = await Promise.all([workspace.getGitIsClean(), workspace.getGitIsModified()]);
+
+    assert(isClean, `Git working tree of workspace "${workspace.name}" is not clean.`);
+    assert(!isModified, `Workspace "${workspace.name}" is modified, but the version is already published.`);
+  }
+
+  // Ensure local production dependencies have been published, and there are no local modifications.
+  await Promise.all(
+    workspace
+      .getLocalDependencies(['dependencies', 'peerDependencies', 'optionalDependencies'])
+      .map(async (dependency) => {
+        // Dependency published successfully during this command invocation.
+        if (await getResult(dependency.name).then((result) => result.isPublished)) return;
+
+        if (isGitRepo) {
+          // Dependency was already published and is unmodified.
+          if (!(await dependency.getGitIsModified())) return;
+        } else {
+          // Dependency was already published.
+          if (!(await dependency.getNpmIsPublished())) return;
+        }
+
+        throw new Error(`Local dependency "${dependency.name}@${dependency.version}" has unpublished modifications.`);
+      }),
+  );
+
+  const patch: MutablePackageJson = {};
+
+  if (isGitRepo) {
+    // Register cleanup of temporary changes to the package.json file.
+    workspacesToRestore.push(workspace);
+
+    // Temporarily set "gitHead" in the package.json file. NPM publish
+    // should do this automatically. But, it doesn't do it for packing.
+    // It's also not documented well even though it is definitely added
+    // intentionally in v7.
+    patch.gitHead = await workspace.getGitHead();
+  }
+
+  // Temporarily update local dependency versions to real versions. File
+  // (file:) and wildcard versions should not be published to the
+  // registry.
+  for (const scope of ['dependencies', 'peerDependencies', 'optionalDependencies'] as const) {
+    const dependencies = workspace.getLocalDependencies([scope]);
+
+    for (const dependency of dependencies) {
+      // Try to match the existing version range prefix, or default to "^".
+      const rangePrefix = workspace[scope][dependency.name]?.match(/^(|~|^|>=?)[a-zA-Z0-9.-]+$/u)?.[1] ?? '^';
+
+      patch[scope] = {
+        ...patch[scope],
+        [dependency.name]: `${rangePrefix}${dependency.version}`,
+      };
+    }
+  }
+
+  await workspace.patchPackageJson(patch);
+
+  const { toArchive = false, dryRun = false } = opts;
+
+  if (toArchive) {
+    await spawn('npm', ['pack', ...(dryRun ? ['--dry-run'] : [])], { errorEcho: true, errorThrow: true });
+  } else {
+    await spawn('npm', ['publish', ...(dryRun ? ['--dry-run'] : [])], { errorEcho: true, errorThrow: true });
+  }
+
+  return { isPublished: true };
+};
+
+const getResult = async (name: string): Promise<{ isPublished: boolean }> => {
+  const eachPromise = resultPromises.get(name);
+
+  assert(eachPromise, `Workspace order is broken. The "each" hook for "${name}" has not been invoked.`);
+
+  return await eachPromise;
+};
