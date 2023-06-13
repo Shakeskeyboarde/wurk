4ce9d8a0a7b59101a362a0d5fb58a27234dfd897
Chris Ackerman <chris@topher.land>
2023-06-13 12:15:55 -0700

    refactor: patchJsonFile accepts N patches

--- packages/werk/src/utils/patch-json-file.ts
+++ packages/werk/src/utils/patch-json-file.ts
@@ -2,8 +2,8 @@ import { merge } from './merge.js';
 import { readJsonFile } from './read-json-file.js';
 import { writeJsonFile } from './write-json-file.js';
 
-export const patchJsonFile = async (filename: string, patch: unknown): Promise<void> => {
+export const patchJsonFile = async (filename: string, ...patch: unknown[]): Promise<void> => {
   const json = await readJsonFile(filename);
-  const merged = merge(json, patch);
+  const merged = merge(json, ...patch);
   await writeJsonFile(filename, merged);
 };
