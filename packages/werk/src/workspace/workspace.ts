3f26ac46863f43068d68282f4eeedfca7f99816e
Chris Ackerman <chris@topher.land>
2023-06-21 09:50:36 -0700

    chore(cli/workspace): filter out falsy package.json patches

--- packages/werk/src/workspace/workspace.ts
+++ packages/werk/src/workspace/workspace.ts
@@ -163,8 +163,10 @@ export class Workspace {
    * always represents the initial state of the workspace when the
    * command was started.
    */
-  readonly patchPackageJson = async <T extends Record<string, unknown>[]>(...jsonPatches: T): Promise<void> => {
-    await patchJsonFile(join(this.dir, 'package.json'), ...jsonPatches);
+  readonly patchPackageJson = async <T extends (Record<string, unknown> | null | undefined | false | 0 | '')[]>(
+    ...jsonPatches: T
+  ): Promise<void> => {
+    await patchJsonFile(join(this.dir, 'package.json'), ...jsonPatches.filter(Boolean));
   };
 
   /**
