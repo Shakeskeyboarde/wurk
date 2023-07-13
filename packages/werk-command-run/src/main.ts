9367000456a964bcc3d699a242ab59b20ab7238b
Chris Ackerman <chris@topher.land>
2023-07-13 16:35:09 -0700

    BREAKING CHANGE(run)!: Extra args passed to all scripts in a CSV.

--- packages/werk-command-run/src/main.ts
+++ packages/werk-command-run/src/main.ts
@@ -4,51 +4,45 @@ export default createCommand({
   init: ({ commander, command }) => {
     return commander
       .description(command.packageJson.description ?? '')
-      .description(
-        `If a script is not found in a workspace, a warning will be
-        printed, but the command will complete successfully.`,
-      )
+      .description('Scripts are only run if they are present.')
       .description(
         `The script can be a CSV of scripts to run more than one script
-        at a time. Arguments are passed only to the last script. The
+        at a time. Extra arguments are passed to ALL scripts. The
         scripts are run in parallel if global options allow, and the
         --sequential flag is not set.`,
       )
       .argument('<script>', 'Script (or scripts CSV) to run in each workspace.')
-      .argument('[args...]', 'Arguments passed to the script.')
+      .argument('[args...]', 'Arguments passed to scripts.')
       .option('-s, --sequential', 'Run multiple scripts (CSV) sequentially.')
       .passThroughOptions();
   },
 
   before: async ({ args, opts }) => {
-    const [scriptsCsv, scriptArgs] = args;
+    const [scriptsCsv] = args;
     const { sequential = false } = opts;
-    const scripts = scriptsCsv.split(',');
+    const scripts = scriptsCsv
+      .split(',')
+      .map((script) => script.trim())
+      .filter(Boolean);
 
     if (sequential) {
-      return [{ scripts: scripts.map((script) => script.trim()), scriptArgs }];
+      return [{ scripts }];
     }
 
-    return scripts.map((script, i) => ({
-      scripts: [script.trim()],
-      scriptArgs: i === scripts.length - 1 ? scriptArgs : [],
-    }));
+    return sequential ? [{ scripts }] : scripts.map((script) => ({ scripts: [script] }));
   },
 
-  each: async ({ workspace, matrixValue, spawn }) => {
+  each: async ({ args, workspace, matrixValue, spawn }) => {
     if (!workspace.selected) return;
 
-    const { scripts, scriptArgs } = matrixValue;
+    const [, scriptArgs] = args;
+    const { scripts } = matrixValue;
 
-    for (const [i, script] of scripts.entries()) {
-      const exitCode = await spawn(
-        'npm',
-        ['run', '--if-present', script, ...(i === scripts.length - 1 ? scriptArgs : [])],
-        {
-          echo: true,
-          errorReturn: true,
-        },
-      ).getExitCode();
+    for (const script of scripts) {
+      const exitCode = await spawn('npm', ['run', '--if-present', script, ...scriptArgs], {
+        echo: true,
+        errorReturn: true,
+      }).getExitCode();
 
       if (exitCode !== 0) {
         process.exitCode = exitCode;
