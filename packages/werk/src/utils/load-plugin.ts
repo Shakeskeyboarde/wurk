e70567b45ee157cee15083beaa379d829d19cfde
Chris Ackerman <chris@topher.land>
2023-09-12 00:40:43 -0700

    BREAKING CHANGE (cli): Remove support for globally installed command plugins.

--- packages/werk/src/utils/load-plugin.ts
+++ packages/werk/src/utils/load-plugin.ts
@@ -2,13 +2,10 @@ import assert from 'node:assert';
 import { readFile } from 'node:fs/promises';
 import { createRequire } from 'node:module';
 import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { inspect } from 'node:util';
 
-import { getNpmGlobalPackagesRoot } from '../npm/get-npm-global-packages-root.js';
 import { getNpmWorkspacesRoot } from '../npm/get-npm-workspaces-root.js';
 import { log } from './log.js';
-import { memoize } from './memoize.js';
 import { type PackageJson } from './package-json.js';
 
 export interface Plugin {
@@ -18,7 +15,6 @@ export interface Plugin {
   readonly packageJson: PackageJson;
 }
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
 const require = createRequire(import.meta.url);
 
 const readPackageInfo = async (dir: string): Promise<{ packageJson: PackageJson; dir: string } | undefined> => {
@@ -30,53 +26,46 @@ const readPackageInfo = async (dir: string): Promise<{ packageJson: PackageJson;
     });
 };
 
-export const loadPlugin = memoize(
-  async (packageNames: string | readonly string[]): Promise<Plugin | undefined> => {
-    log.silly(`loadPlugin(${inspect(packageNames)})`);
+export const loadPlugin = async (packageNames: string | readonly string[]): Promise<Plugin | undefined> => {
+  log.silly(`loadPlugin(${inspect(packageNames)})`);
 
-    packageNames = Array.isArray(packageNames) ? packageNames : [packageNames];
+  packageNames = Array.isArray(packageNames) ? packageNames : [packageNames];
 
-    const [workspacesRoot, globalPackagesRoot] = await Promise.all([
-      getNpmWorkspacesRoot(),
-      getNpmGlobalPackagesRoot(),
-    ]);
+  const workspacesRoot = await getNpmWorkspacesRoot();
+  const paths = [resolve(workspacesRoot, 'node_modules')];
 
-    const paths = [resolve(workspacesRoot, 'node_modules'), globalPackagesRoot, __dirname];
-    let resolved: { exports: Record<string, unknown>; main: string } | undefined;
+  let resolved: { exports: Record<string, unknown>; main: string } | undefined;
 
-    for (const packageName of packageNames) {
-      try {
-        const main = require.resolve(packageName, { paths });
-        const exports = await import(main);
-        resolved = { exports, main };
-        break;
-      } catch (error) {
-        if (
-          error instanceof Error &&
-          'code' in error &&
-          typeof error.code === 'string' &&
-          error.code?.includes('MODULE_NOT_FOUND')
-        ) {
-          continue;
-        }
-
-        throw new Error(
-          `Failed loading plugin "${packageName}".${error instanceof Error ? `\n${error.message}` : ''}`,
-          { cause: error },
-        );
+  for (const packageName of packageNames) {
+    try {
+      const main = require.resolve(packageName, { paths });
+      const exports = await import(main);
+      resolved = { exports, main };
+      break;
+    } catch (error) {
+      if (
+        error instanceof Error &&
+        'code' in error &&
+        typeof error.code === 'string' &&
+        error.code?.includes('MODULE_NOT_FOUND')
+      ) {
+        continue;
       }
+
+      throw new Error(`Failed loading plugin "${packageName}".${error instanceof Error ? `\n${error.message}` : ''}`, {
+        cause: error,
+      });
     }
+  }
 
-    if (!resolved) return;
+  if (!resolved) return;
 
-    assert(
-      typeof resolved.exports === 'object' && resolved.exports !== null,
-      'Plugin import() did not return an object.',
-    );
+  assert(
+    typeof resolved.exports === 'object' && resolved.exports !== null,
+    'Plugin import() did not return an object.',
+  );
 
-    const packageInfo = await readPackageInfo(dirname(resolved.main));
+  const packageInfo = await readPackageInfo(dirname(resolved.main));
 
-    return packageInfo && { ...resolved, ...packageInfo };
-  },
-  (packageNames) => JSON.stringify(packageNames),
-);
+  return packageInfo && { ...resolved, ...packageInfo };
+};
