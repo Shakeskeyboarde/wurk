13c438cf728f564000608c4d40d56fc2fdb15105
Chris Ackerman <chris@topher.land>
2023-09-17 16:50:00 -0700

    improvement: Add 'ls' alias to the list command.

--- packages/werk-command-list/src/main.ts
+++ packages/werk-command-list/src/main.ts
@@ -1,6 +1,9 @@
 import { createCommand } from '@werk/cli';
 
 export default createCommand({
+  config: (commander) => {
+    return commander.alias('ls');
+  },
   after: async ({ log, workspaces }) => {
     log.info(
       JSON.stringify(
