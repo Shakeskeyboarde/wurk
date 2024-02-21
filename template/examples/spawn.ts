5be153b6fef588cb04d6a0c65cbb59208bfee0d9
Chris Ackerman <chris@topher.land>
2024-02-21 15:02:56 -0800

    improve(template): spawn example

--- template/examples/spawn.ts
+++ template/examples/spawn.ts
@@ -2,13 +2,11 @@ import { createCommand } from 'wurk';
 
 export default createCommand('spawn-example', {
   run: async ({ workspaces }) => {
-    await workspaces.forEach(async ({ log, name, dir, spawn }) => {
-      log.info(`git status for workspace "${name}"`);
-
+    await workspaces.forEach(async ({ log, dir, spawn }) => {
       const status = await spawn('git', ['status', '--porcelain', '--', dir]).stdoutText();
 
       if (status.length !== 0) {
-        log.warn(`workspace "${name}" has uncommitted changes`);
+        log.warn(`workspace has dirty working tree`);
       }
     });
   },
