a963d1ec70693e7200d9686413111f60a5f33796
Chris Ackerman <chris@topher.land>
2024-02-26 11:30:19 -0800

    fix: vite test

--- tests/vite/src/index.tsx
+++ tests/vite/src/index.tsx
@@ -1,5 +1,10 @@
+import { type FC, type ReactNode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+const Component: FC<{ children?: ReactNode }> = ({ children }) => (
+  <div>{children}</div>
+);
+
 createRoot(document.body.appendChild(document.createElement('div'))).render(
-  <div>Hello, world!</div>,
+  <Component>Hello, world!</Component>,
 );
